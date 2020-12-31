6caa4d58e796914e07c61330cff931832821be8a
erfantbtb <76693574+erfantbtb@users.noreply.github.com>
2020-12-31 08:12:56 -0800

    ghesmate shekl dasti kamel

--- static project(matplotlib).py
+++ static project(matplotlib).py
@@ -7,19 +7,42 @@ Created on Mon Dec 28 06:47:42 2020
 import matplotlib.pyplot as plt
 import numpy as np
 
-pos = []
-def onclick(event):
-    """jamavarie mokhtasate noghat daroone yek list"""
-    pos.append([event.xdata, event.ydata])
-    plt.scatter(event.xdata, event.ydata, s=10)
-    print([event.xdata, event.ydata])
-    print(len(pos))
+pos_x = []
+pos_y = []
+
+def start_draw(event):
+    """jamavarie mokhtasate noghat daroone yek list va rasme shekl"""
+     
+    pos_x.append(event.xdata)
+    pos_y.append(event.ydata)
+    plt.scatter(pos_x, pos_y)
     
-f,a = plt.subplots()
+    """keshidane khotoot va kamel kardane shekl"""
+    
+    if len(pos_x)>1 and len(pos_y)>1:
+        
+        if abs(event.xdata-pos_x[0]) > 0.05*pos_x[0] or abs(event.ydata-pos_y[0]) > 0.05*pos_y[0]:
+            plt.plot(pos_x, pos_y)
+            
+        else:
+            plt.plot([pos_x[-2], pos_x[0]], [pos_y[-2], pos_y[0]])    
+            fig.canvas.mpl_disconnect(button)
+            
+    print([event.xdata, event.ydata])
+    
+"""moshakhasate nemoodar""" 
+   
+fig,axis = plt.subplots()
+
 plt.xlim(0,10)
 plt.xticks(np.arange(11))
+
 plt.ylim(0,10)
 plt.yticks(np.arange(11))
+
 plt.grid()
-f.canvas.mpl_connect('button_press_event',onclick)
-f.show()    
+
+"""mouse click event for getting points"""
+
+button = fig.canvas.mpl_connect('button_press_event',start_draw)
+fig.show()    
