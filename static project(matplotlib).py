cfd771c0a0e4b508dcff0052f77927f8a3f8c63b
Ahmadmoo <66487994+Ahmadmoo@users.noreply.github.com>
2021-01-02 14:01:32 +0330

    Update static project(matplotlib).py

--- static project(matplotlib).py
+++ static project(matplotlib).py
@@ -21,7 +21,7 @@ def start_draw(event):
     
     if len(pos_x)>1 or len(pos_y)>1:
         
-        if abs(event.xdata-pos_x[0]) > 0.05*pos_x[0] and abs(event.ydata-pos_y[0]) > 0.05*pos_y[0]:
+        if abs(event.xdata-pos_x[0]) > 0.05*pos_x[0] or abs(event.ydata-pos_y[0]) > 0.05*pos_y[0]:
             plt.plot(pos_x, pos_y)
             
         else:
