3b22ffe798c52c82629d1cdb42474ba78876084e
erfantbtb <76693574+erfantbtb@users.noreply.github.com>
2021-01-03 09:37:04 -0800

    Add files via upload

--- static project(matplotlib).py
+++ static project(matplotlib).py
@@ -12,6 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 """tarife yek panjere baraye app"""
+
 window = Tk()
 window.title('center of the mass')
 window.geometry('500x300')
@@ -33,24 +34,35 @@ def graph():
         """jamavarie mokhtasate noghat daroone yek list va rasme shekl"""
         pos_x.append(event.xdata)
         pos_y.append(event.ydata)
-        plt.scatter(pos_x, pos_y)
-        plt.draw()
+    
         """keshidane khotoot va kamel kardane shekl"""
-        if len(pos_x)>1 and len(pos_y)>1:
-            if abs(event.xdata-pos_x[0]) > 0.05*pos_x[0] or abs(event.ydata-pos_y[0]) > 0.05*pos_y[0]:
-                plt.plot(pos_x, pos_y)
+        
+        if len(pos_x)>1:
+            plt.scatter(pos_x, pos_y, color ="red")
+            plt.draw()
+            if abs(event.xdata-pos_x[0]) > 0.05*pos_x[0] or \
+            abs(event.ydata-pos_y[0]) > 0.05*pos_y[0]:
+                plt.plot(pos_x, pos_y, color = "dodgerblue")
+                
             else:
-                plt.plot([pos_x[-2], pos_x[0]], [pos_y[-2], pos_y[0]])    
+                plt.scatter(pos_x[0], pos_y[0], color ="red")
+                plt.draw()
+                plt.plot([pos_x[-2], pos_x[0]], [pos_y[-2], pos_y[0]], 
+                         color = "dodgerblue")    
                 fig.canvas.mpl_disconnect(click)
-            
-            print([event.xdata, event.ydata])
+                pos_x.pop()
+                pos_y.pop()
+                pos_x.append(pos_x[0])
+                pos_y.append(pos_y[0])
+                plt.fill(pos_x, pos_y, color = "cyan") 
+
             
     click = fig.canvas.mpl_connect('button_press_event',start_draw)
    
 draw_button = ttk.Button(window, text = 'draw', command = graph)
-draw_button.place(x = 150, y = 200)
+draw_button.place(x = 100, y = 200)
 
-"""gereftane noghat be soorate file"""
+"""gereftane noghat be soorate file(excel, txt)"""
 
 def get_file_address():
     """baz kardane file va rikhtane an dar list"""
@@ -60,6 +72,54 @@ def get_file_address():
         print(position)
         
 get_file_btn = ttk.Button(window, text = "File", command = get_file_address)
-get_file_btn.place(x = 300, y = 200)
+get_file_btn.place(x = 350, y = 200)
+
+
+
+"""gereftan be soorate dasti(shekl haye moshakhas)"""
+
+def open_shapes():
+    
+    fig, axis = plt.subplots()
+    plt.xlim(0,10)  
+    plt.xticks(np.arange(11))
+    plt.ylim(0,10)
+    plt.yticks(np.arange(11))
+    plt.grid()
+    
+    top = Toplevel()
+    top.geometry("400x200")
+    top.title('Common Shapes')
+    
+    voroodi = Entry(top, width = 50)
+    voroodi.place(x = 150, y = 0)
+    
+    shape_box = Listbox(top)
+    shape_box.insert(0, 'Circle')
+    shape_box.insert(1, 'Rectangle')
+    shape_box.insert(2, 'square')
+    shape_box.insert(3, 'triangle')
+    shape_box.insert(4, 'section')
+    shape_box.place(x = 150, y = 100)
+    
+    def to_int(a):
+        list_string = a.split(',')
+        list_string.remove('(')
+        list_string.remove(')')
+        for x in list_string:
+            list_adad = []
+            list_adad.append(int(x))
+        return list_adad
+    
+    def circle():
+        khosoosiat = to_int(voroodi.get())
+        dayere = plt.Circle((khosoosiat[0], khosoosiat[1]), khosoosiate[2])
+        axis.add_artist(dayere)
+    
+    def triangle():
+        
+         
+get_shape_btn = ttk.Button(window, text = 'shape', command = open_shapes)
+get_shape_btn.place(x = 225, y = 200)  
   
 window.mainloop()
